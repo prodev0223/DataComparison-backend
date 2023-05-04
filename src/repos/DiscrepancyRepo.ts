39da8391e452d03bc085f0c728ef0c3874df0c31
Veljko Dimcic <websharp950223@gmail.com>
2023-05-04 19:00:31 +0300

    update(repos): update source format

--- src/repos/DiscrepancyRepo.ts
+++ src/repos/DiscrepancyRepo.ts
@@ -1,21 +1,54 @@
 // **** Functions **** //
 import { readJSONSync } from "fs-extra";
 import { Player } from "@src/models/player";
+
 /* #region Parse data for source.json */
+/**
+ * 
+ * @param data from statistic home/away
+ * @returns 
+ */
 const parseHomeAwayFromSource = (data: any)=> {
-    return {
-        rushAttempts: 0,
-        rushTds: 0,
-        rushYdsGained: 0,
-        rec: 0,
-        receivingYards: 0,
-    }
+  let rushingObj = data.rushing.totals;
+  let receivingObj = data.receiving.totals;
+  return {
+      id: data.id,
+      rushAttempts: rushingObj.attempts,
+      rushTds: rushingObj.touchdowns,
+      rushYdsGained: rushingObj.yards,
+      rec: receivingObj.receptions,
+      receivingYards: receivingObj.yards,
+  }
+}
+
+const checkPlayerExist = (players: Player[] , _player: Player)=> {
+  players.forEach((player, index)=>{
+    if(player.id === _player.id)return index;
+  })
+
+  return -1;
 }
 
 const parsePlayerFromSource = (data: any)=> {
-  var arrUser: Player[] = [];
+  let players: Player[] = [];
+  let rushingPlayers = data.rushing.players;
+  let receivingPlayers = data.receiving.players;
 
-  return arrUser;
+  for(let player of rushingPlayers){
+      players.push(player)
+  }
+
+  for(let player of receivingPlayers){
+      let checkExist = checkPlayerExist(players, player);
+      
+      if(checkExist>=0){
+          players[checkExist] = {...players[checkExist], ...player}
+      }else{
+          players.push(player);
+      }
+  }
+
+  return players;
 }
 
 const parseGameFromSource = (data: any)=> {
@@ -28,17 +61,34 @@ const parseGameFromSource = (data: any)=> {
 
 /* #region Parse data for external.json */
 const parseHomeAwayFromExternal = (data: any)=> {
-    return {
-        rushAttempts: 0,
-        rushTds: 0,
-        rushYdsGained: 0,
-        rec: 0,
-        receivingYards: 0,
-    }
+  return {
+      id: data.id,
+      rushAttempts: data.rushAttempts,
+      rushTds: data.rushTds,
+      rushYdsGained: data.rushYdsGained,
+      rec: data.rec,
+      receivingYards: data.receivingYards,
+  }
 }
 
 const parsePlayerFromExternal = (data: any)=> {
+    let players: Player[] = [];
+    let rushingPlayers = data.rushing.players;
+    let receivingPlayers = data.receiving.players;
 
+    for(let player of rushingPlayers){
+        players.push(player)
+    }
+
+    for(let player of receivingPlayers){
+        let checkExist = checkPlayerExist(players, player);
+        if(checkExist>=0){
+            players[checkExist] = {...players[checkExist] , ...player}
+        }else{
+            players.push(player);
+        }
+    }
+    return players;
 }
 
 const parseGameFromExternal = (data: any)=> {
@@ -60,7 +110,8 @@ const parseSourceInputToCompareFormat = async(inputData: any , mode = 0)=> {
     var newStatistic = {
         home: parseHomeAwayFromSource(statistics.home),
         away: parseHomeAwayFromSource(statistics.away),
-        players: parsePlayerFromSource(statistics),
+        homePlayers: parsePlayerFromSource(statistics.home),
+        awayPlayers: parsePlayerFromSource(statistics.away),
         game: parseGameFromSource(game)
     };
 
@@ -75,7 +126,15 @@ const parseSourceInputToCompareFormat = async(inputData: any , mode = 0)=> {
  * @returns 
  */
 const parseExternalInputToCompareFormat = async(inputData: any, mode=0)=> {
-    return inputData;
+	const { game } = inputData;
+	let newStatistic = {
+		home: parseHomeAwayFromExternal(game.home),
+		away: parseHomeAwayFromExternal(game.away),
+		homePlayers: parsePlayerFromExternal(game.home),
+		awayPlayers: parsePlayerFromExternal(game.away),
+		game: parseGameFromExternal(game)
+	};
+	return newStatistic;
 }
 
 /**
@@ -84,10 +143,10 @@ const parseExternalInputToCompareFormat = async(inputData: any, mode=0)=> {
 const getDataByField = async(mode = 0)=> {
     const externalData = readJSONSync(__dirname + '/' + 'external.json');
     const sourceData = readJSONSync(__dirname + '/' + 'source.json');
-    const newExternalData = parseSourceInputToCompareFormat(externalData , mode);
-    const newSourceData = parseExternalInputToCompareFormat(sourceData , mode);
+	const newExternalData = parseExternalInputToCompareFormat (externalData , mode);
+    const newSourceData = parseSourceInputToCompareFormat(sourceData , mode);
 
-    return newSourceData;
+    return newExternalData;
 }
 
 /**
